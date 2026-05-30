976c14186c7c16dd6f27482293f317edd470786a
Ashutosh <ashutoshzacb@gmail.com>
2026-05-30 21:26:04 +0530

    Fixed deployed backend CORS

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
+CORS(app, origins="*")
+@app.after_request
+def after_request(response):
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add("Access-Control-Allow-Headers", "Content-Type,Authorization")
+    response.headers.add("Access-Control-Allow-Methods", "GET,POST,OPTIONS")
+    return response
 
 UPLOAD_FOLDER = "uploads"
 
